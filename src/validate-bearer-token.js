02d70c0abb485457c949b71b237821e4a9c973e0
Olivia Davis <olivia.davis593@gmail.com>
2020-04-07 00:09:32 -0500

    unauthorized request validation testing

--- src/validate-bearer-token.js
+++ src/validate-bearer-token.js
@@ -7,7 +7,7 @@ function validateBearerToken(req, res, next) {
     const authToken = req.get('Authorization')
 
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
-        return res.status(401).json({ error: 'Unauthorized request' })
+        return res.status(401).json({ error: `Unauthorized request` })
     }
 
     //move to next middleware 
--- test/bookmarks-endpoints.spec.js
+++ test/bookmarks-endpoints.spec.js
@@ -27,6 +27,43 @@ describe('Bookmarks Endpoints', function() {
     //afterEach() => removes any table data so next test has clean start 
     afterEach('cleanup', () => db('bookmarks').truncate()) 
 
+    describe(`Unauthorized requests`, () => {
+        const testBookmarks = fixtures.makeBookmarksArray()
+
+        beforeEach('insert bookmarks', () => {
+            return db
+                .into('bookmarks')
+                .insert(testBookmarks)
+        })
+
+        it(`responds with 401 unauthorized for GET /bookmarks`, () => {
+            return supertest(app)
+                .get('/bookmarks')
+                .expect(401, { error: 'Unauthorized request'})
+        })
+
+        it(`responds with 401 Unauthorized POST /bookmarks`, () => {
+            return supertest(app)
+                .post('/bookmarks')
+                .send({ title: 'test-title', url: 'http://some.thing.com', rating: 1})
+                .expect(401, { error: 'Unauthorized request' })
+        })
+
+        it(`responds with 401 Unauthorized GET /bookmarks/:bookmark_id`, () => {
+            const secondBookmark = testBookmarks[1]
+            return supertest(app)
+                .get(`/bookmarks/${secondBookmark.id}`)
+                .expect(401, { error: 'Unauthorized request' })
+        })
+
+        it(`responds with 401 Unauthorized for DELETE /bookmarks/:bookmark_id`, () => {
+            const aBookmark = testBookmarks[1]
+            return supertest(app)
+                .delete(`/bookmarks/${aBookmark.id}`)
+                .expect(401, { error: 'Unauthorized request'})
+        })
+    })
+
     describe(`GET /bookmarks`, () => {
         context(`Given no bookmarks`, () => {
             it(`responds with 200 and an empty list`, () => {
